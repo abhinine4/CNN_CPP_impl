c2cc0de05db0eac710bef4c893dd3c502d298e38
Abhishek Kumar <akumar58@buffalo.edu>
2023-10-11 17:15:35 -0400

    added Conv and pooling impl

--- Layer/Pooling.h
+++ Layer/Pooling.h
@@ -39,10 +39,68 @@ class Pooling :public Layer{
         void init(const Scalar& mu, const Scalar& sigma, RNG& rng){}
 
         void forward(const Matrix& prev_layer_data){
-            
+            const int nobs = prev_layer_data.cols();
+            m_loc.resize(this->m_out_size, nobs);
+            m_z.resize(this->m_out_size, nobs);
+
+            int* loc_data = m_loc.data();
+            const int channel_end = prev_layer_data.size();
+            const int channel_stride = m_channel_rows * m_channel_cols;
+            const int col_end_gap = m_channel_rows * m_pool_cols * m_ou_cols;
+            const int col_stride = m_channel_rows * m_pool_cols;
+            const int row_end_gap = m_out_rows * m_pool_rows;
+
+            for(int channel_start = 0; channel_start < channel_end; channel_start += channel_stride){
+                const int col_end = channel_start + col_end_gap;
+
+                for(int col_start = channel_start; col_start < col_end; col_start += col_stride){
+                    const int row_end = col_start + row_end_gap;
+
+                    for(int row_start = col_start; row_start < row_end; row_start += m_pool_rows, loc_data++){
+                        *loc_data = row_start;
+                    }
+                }
+            }
+
+            loc_data = m_loc.data();
+            const int* const loc_end = loc_data + m_loc.size();
+            Scalar* z_data = m_z.data();
+
+            for(; loc_data < loc_end; loc_data++, z_data++){
+                const int offset = *loc_data;
+                *z_data = internal::find_block_max(src + offset, m_pool_rows, m_pool_cols, m_channel_rows, *loc_data);
+                *loc_data += offset;
+            }
+
+            m_a.resize(this->m_out_size, nobs);
+            Activation::activate(m_z, m_a);
         }
 
+        const Matrix& output() const{
+            return m_a;
+        }
 
+        void backprop(const matrix& prev_layer_data, const Matrix& next_layer_data){
+            const int nobs = prev_layer_data.cols();
+            Matrix& dLz = m_z;
+            Activation::apply_jacobian(m_z, m_a, next_layer_data, dLz);
 
+            m_din.resize(this->m_in_size, nobs);
+            m_din.setZero();
+            const int dLz_size = dLz.size();
+            const Scalar* dLz_data = dLz.data();
+            const int* loc_data = m_loc.data();
+            Scalar* din_data = m_din.data();
+
+            for(int i=0; i <dLz_size; i++){
+                din_data[loc_data[i]] += dLz_data[i];
+            }
+        }
+
+        const Matrix& backprop_data() const{
+            return m_din;
+        }
+
+        const update(Optimizer& opt){}
 
 };
